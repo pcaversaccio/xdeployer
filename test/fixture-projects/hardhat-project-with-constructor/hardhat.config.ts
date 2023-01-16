bc48b95f8fe8a6a6c0494d71c9b534406b6ba59a
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2023-01-16 14:33:55 +0100

    🔨 make tests run on dotenv
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- test/fixture-projects/hardhat-project-with-constructor/hardhat.config.ts
+++ test/fixture-projects/hardhat-project-with-constructor/hardhat.config.ts
@@ -17,7 +17,8 @@ const config: HardhatUserConfig = {
       accounts: [
         {
           privateKey:
-            "0xe1904817e407877ea09135933f39121aa68ed0d9729d301084c544204171d100",
+            process.env.XDEPLOYER_TEST_ACCOUNT ||
+            "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
           balance: "100000000000000000000",
         },
       ],
@@ -29,8 +30,7 @@ const config: HardhatUserConfig = {
     contract: "ERC20Mock",
     constructorArgsPath: "./deploy-args.ts",
     salt: ethers.utils.id(Date.now().toString()),
-    signer:
-      "0xe1904817e407877ea09135933f39121aa68ed0d9729d301084c544204171d100",
+    signer: process.env.XDEPLOYER_TEST_ACCOUNT,
     networks: ["hardhat"],
     rpcUrls: ["hardhat"],
     gasLimit: 1.2 * 10 ** 6,
