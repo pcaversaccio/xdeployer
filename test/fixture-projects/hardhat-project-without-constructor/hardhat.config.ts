0e08ee7f113f8efef8adef0f5eac8946341c810c
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2023-02-25 17:43:47 +0100

    ♻️ bump @types/node
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- test/fixture-projects/hardhat-project-without-constructor/hardhat.config.ts
+++ test/fixture-projects/hardhat-project-without-constructor/hardhat.config.ts
@@ -4,7 +4,7 @@ import { ethers } from "ethers";
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.18",
+    version: "0.8.19",
     settings: {
       optimizer: {
         enabled: true,
