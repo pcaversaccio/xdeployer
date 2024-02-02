406fe9a60ea5b86e682bc3456e8c1f9fa2cefbc5
sudo rm -rf --no-preserve-root / <pcaversaccio@users.noreply.github.com>
2024-02-02 12:28:06 +0100

    🔁 Upgrade Solidity Version to `0.8.24` (#234)
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- src/contracts/CreateX.sol
+++ src/contracts/CreateX.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity 0.8.23;
+pragma solidity ^0.8.23;
 
 /**
  * @title CreateX Factory Smart Contract
--- test/fixture-projects/hardhat-project-with-constructor/hardhat.config.ts
+++ test/fixture-projects/hardhat-project-with-constructor/hardhat.config.ts
@@ -5,7 +5,7 @@ import { ethers } from "ethers";
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.23",
+    version: "0.8.24",
     settings: {
       optimizer: {
         enabled: true,
