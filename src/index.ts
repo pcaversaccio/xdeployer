99b8e3bb560ca4b2aaeef77440c2f956b4e9e4f4
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2024-05-01 12:41:51 +0200

    ♻️ Prettify
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- src/index.ts
+++ src/index.ts
@@ -167,7 +167,8 @@ task(
                     : `${explorers[idx]}tx/${createReceipt[i].hash}`,
               address: computedContractAddress,
               addressLink:
-                hre.config.xdeploy.networks[i].slice(0, 16) == "seiArcticTestnet"
+                hre.config.xdeploy.networks[i].slice(0, 16) ==
+                "seiArcticTestnet"
                   ? `${explorers[idx]}account/${computedContractAddress}`
                   : `${explorers[idx]}address/${computedContractAddress}`,
               receipt: createReceipt[i].toJSON(),
