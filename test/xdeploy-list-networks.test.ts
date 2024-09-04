b1d1729738b8a750c05e489a7a3ca734957118e9
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2024-09-04 16:51:51 +0200

    ♻️ Refactor
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- test/xdeploy-list-networks.test.ts
+++ test/xdeploy-list-networks.test.ts
@@ -2,7 +2,7 @@ import { useEnvironment } from "./helpers";
 
 describe("Plugin test xdeploy: Printing the supported networks table", function () {
   useEnvironment("hardhat-project-with-constructor");
-  it("list supported network table successfully", async function () {
+  it("list supported networks table successfully", async function () {
     return this.hre.run("xdeploy", { listNetworks: true });
   });
 });
