2926e444f54dce05d4fb3fdc7bb85d1fdac15186
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2025-03-16 13:11:26 +0100

    ♻️ Upgrade to Solidity Version `0.8.29`
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- test/fixture-projects/sepolia-holesky-project-with-constructor/hardhat.config.ts
+++ test/fixture-projects/sepolia-holesky-project-with-constructor/hardhat.config.ts
@@ -5,7 +5,7 @@ import { ethers } from "ethers";
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.28",
+    version: "0.8.29",
     settings: {
       optimizer: {
         enabled: true,
