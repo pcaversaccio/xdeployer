cade08bb32336cbf59f6de81adf73e4bcfc399e4
pcaversaccio <pascal.caversaccio@hotmail.ch>
2026-07-14 12:44:43 +0200

    ♻️ Upgrade to Solidity `0.8.36`
    Signed-off-by: pcaversaccio <pascal.caversaccio@hotmail.ch>

--- test/fixture-projects/hardhat-project-with-constructor/hardhat.config.ts
+++ test/fixture-projects/hardhat-project-with-constructor/hardhat.config.ts
@@ -5,7 +5,7 @@ import { ethers } from "ethers";
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.35",
+    version: "0.8.36",
     settings: {
       optimizer: {
         enabled: true,
